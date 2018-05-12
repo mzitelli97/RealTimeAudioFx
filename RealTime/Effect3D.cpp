ced2c5766c2b1882c852c83327f0af693832ae39
Diego Juarez <dijuarez>
2018-05-12 17:12:11 -0300

    Effect 3D con un bloque solo andando

--- RealTime/Effect3D.cpp
+++ RealTime/Effect3D.cpp
@@ -10,8 +10,8 @@ bool Effect3D::next(const void * inputBuffer, void * outputBuffer, unsigned long
 	std::vector<float> leftOutput = std::vector<float>((unsigned)framesPerBuffer, 0);
 	std::vector<float> rightOutput = std::vector<float>((unsigned)framesPerBuffer, 0);
 	
-	hsFilter(inputBuffer,&leftOutput , framesPerBuffer, props[0].getValue());
-	hsFilter(inputBuffer, &rightOutput, framesPerBuffer, props[1].getValue());
+	hsFilter(inputBuffer,&leftOutput , framesPerBuffer, props[0].getValue(),'L');
+	hsFilter(inputBuffer, &rightOutput, framesPerBuffer, 180-props[0].getValue(),'R');
 	float *out = (float*)outputBuffer;
 	float *in = (float*)inputBuffer;
 	//el output buffer para estereo va intercalando left y right
@@ -29,9 +29,14 @@ bool Effect3D::next(const void * inputBuffer, void * outputBuffer, unsigned long
 
 Effect3D::Effect3D():Effect("3D")
 {
-	props = { Properties(std::string("ThetaLeft"),0,360),Properties(std::string("ThetaRight"),0,360) };
+	props = { Properties(std::string("LeftAngle"),0,360) };
 	props[0].setValue(0);
-	props[1].setValue(180);
+	prevHSoutL = 0;
+	prevHSoutL = 0;
+	prevInL = 0;
+	prevInR = 0;
+	prevOutL = 0;
+	prevOutR = 0;
 
 }
 
@@ -46,7 +51,7 @@ bool Effect3D::setProp(unsigned i, double v)
 	return ret;
 }
 
-bool Effect3D::hsFilter(const void * inputBuffer, std::vector<float> * outputBuffer, unsigned long framesPerBuffer, float theta)
+bool Effect3D::hsFilter(const void * inputBuffer, std::vector<float> * outputBuffer, unsigned long framesPerBuffer, float theta, char channel)
 {
 	float Fs = sampleRate;
 	float thetaO = 150;
@@ -62,27 +67,44 @@ bool Effect3D::hsFilter(const void * inputBuffer, std::vector<float> * outputBuf
 	
 	std::vector<float> buff= std::vector<float>((unsigned)framesPerBuffer, 0);
 	std::vector<float>::iterator tempIt=buff.begin();
+
+	
+	float* prevIn = &prevInR;
+	float* prevHSout = &prevHSoutR;
+	float* prevOut = &prevOutR;
+	
+	if (channel == 'L')
+	{
+		prevIn = &prevInL;
+		prevHSout = &prevHSoutL;
+		prevOut = &prevOutL;
+	}
+	
 	
 	/*--------Head shadow---------*/
-	*tempIt++ = (*in++)*(wo +Fs);
+	*tempIt++ = (-(wo - Fs)*(*prevHSout) + (*in++)*(wo + alpha * Fs) + (wo - alpha * Fs)*(*prevIn)) / (wo + Fs);
 	//*tempIt++ = *in++;
 	for (unsigned long i = 0; i < framesPerBuffer - 1; i++, tempIt++, in++)
 		//*tempIt = *in;
-		*tempIt =( -(wo - Fs)*(*(tempIt - 1)) + (*in)*(wo +alpha*Fs) + (wo - alpha * Fs)*(*(in - 1)) )/(wo+Fs);
-
+		*tempIt =( -(wo - Fs)*(*(tempIt - 1)) + (*in)*(wo +alpha*Fs) + (wo - alpha * Fs) * (*(in - 1)) )/(wo+Fs);
+	
+	*prevIn = *(--in);
+	/*-------------Group Delay-------------------*/
 	if (abs(theta) < 90)
 		gDelay = -(Fs /wo)*(cos(theta*PI / 180) - 1);
 	else
 		gDelay = (Fs /wo )* ((abs(theta) - 90)*PI / 180 + 1);
-	
+
 	float gCoef = (1-gDelay)/(1+gDelay);
 	/*------- IDT ---------------*/
 	tempIt = buff.begin();
 	//*out++ = *tempIt++;
-	*out++ = gCoef *(*tempIt++);
+	*out++ = -gCoef * (*prevOut) + gCoef * (*(tempIt++)) + *prevHSout;
 	for (unsigned long i = 0; i < framesPerBuffer - 1; i++, out++, tempIt++)
 		//*out = *tempIt;
 		*out = -gCoef * (*(out - 1)) + gCoef * (*tempIt) + *(tempIt - 1);
+	*prevHSout = *(--tempIt);
+	*prevOut = *(--out);
 
 	return false;
 }
@@ -93,3 +115,17 @@ bool Effect3D::hsFilter(const void * inputBuffer, std::vector<float> * outputBuf
 Effect3D::~Effect3D()
 {
 }
+
+
+
+	/*
+	float* prevIn = &prevInR;
+	float* prevHSout = &prevHSoutR;
+	float* prevOut = &prevOutR;
+	/*--------Head shadow---------*/
+	/*if (channel == 'L')
+	{
+		prevIn = &prevInL;
+		prevHSout = &prevHSoutL;
+		prevOut = &prevOutL;
+	}*/
